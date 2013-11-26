0fb9e98e091db1e8f55ca8d46debd089fb5a6bd6
SmallAiTT <536762164@qq.com>
2013-11-26 22:21:28 +0800

    模块划分

--- modules/demo/ctrl/curdCtrl.js
+++ modules/demo/ctrl/curdCtrl.js
@@ -1,30 +1,29 @@
+var ttUtils = require("tt-utils");
+var Curd = require("../do/Curd");
+var ttData = require("tt-data");
+var curdUrl = ttData.DEMO_CTRL.curd;
+
 module.exports = function(app, ctrlName){
-
-    var CTRL_C = ctrlName + "create";
-    var CTRL_U = ctrlName + "update";
-    var CTRL_R = ctrlName + "read";
-    var CTRL_D = ctrlName + "del";
-    var CTRL_L = ctrlName + "list";
-
-    app.post(CTRL_C, function(req, res){
-        return app.doInfo(res);
+    app.post(curdUrl.create, function(req, res){
+        var curd = ttUtils.transReq(req, Curd);
+        return ttMsg.doInfo(res, curd);
         app.gmClient.insert("insert into demo_curd set ? ",
             {code : 1, name : "Name1", sel : "T", date : new Date()},
             function(err, result){
-                if(err) return app.doErr(res, err);
-                app.doInfo(res);
+                if(err) return ttMsg.doErr(res, err);
+                ttMsg.doInfo(res);
         });
     });
-    app.post(CTRL_U, function(req, res){
+    app.post(curdUrl.update, function(req, res){
 
     });
-    app.post(CTRL_R, function(req, res){
+    app.post(curdUrl.read, function(req, res){
 
     });
-    app.post(CTRL_D, function(req, res){
+    app.post(curdUrl.del, function(req, res){
 
     });
-    app.post(CTRL_L, function(req, res){
+    app.post(curdUrl.list, function(req, res){
         res.json([{
             id : 1, code : 1, name : "Name1", sel : [], date : "2011/11/11"
         }]);
